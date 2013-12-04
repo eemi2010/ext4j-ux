23ead4d7c0753f29951690cdb3d52cd7c5629c4a
eemi2010 <jazzmatadazz@gmail.com>
2013-12-05 00:15:02 +0100

    Initial commit

---
+++ ext4j-ux/src/com/eemi/extj/ux/app/client/Ext4jUx.java
@@ -0,0 +1,15 @@
+package com.eemi.extj.ux.app.client;
+
+import com.eemi.ext4j.client.core.ExtEntryPoint;
+
+/**
+ * Entry point classes define <code>onLoad()</code>.
+ */
+public class Ext4jUx extends ExtEntryPoint {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+}
---
+++ ext4j-ux/src/com/eemi/extj/ux/client/Ext4jUx.java
@@ -0,0 +1,5 @@
+package com.eemi.extj.ux.client;
+
+public interface Ext4jUx {
+    public void injectUx();
+}
